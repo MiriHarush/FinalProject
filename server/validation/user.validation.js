0d0fd5aa996fd0d497930012c86d0d38865fd884
MiriHarush <132377898+MiriHarush@users.noreply.github.com>
2023-12-25 14:20:57 +0200

    validate

---
+++ server/validation/user.validation.js
@@ -0,0 +1,28 @@
+const Joi = require("joi")
+
+exports.validCreateUser = (userBody) => {
+    const signUpSchema =
+        Joi.object().keys({
+            name: Joi.string().required(),
+            userName: Joi.string().required(),
+            phone: Joi.string().required(),
+            password: Joi.string()
+                // .max(20).required(),
+                .regex(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/)
+                .required()
+                .messages({
+                    'string.pattern.base': 'Password must contain at least one uppercase letter, one lowercase letter, and one number.'
+                }),
+            email: Joi.string().email({ tlds: { allow: ['com', 'net', 'org'] } }).error(() => Error('Email is not valid')).required(),
+        })
+    return signUpSchema.validate(userBody);
+}
+
+exports.validLogIn = (userBody) => {
+    const logInSchema =
+        Joi.object().keys({
+            password: Joi.string(),
+            email: Joi.string().email({ tlds: { allow: ['com', 'net', 'org'] } }).error(() => Error('Email is not valid')),
+        })
+    return logInSchema.validate(userBody)
+}
