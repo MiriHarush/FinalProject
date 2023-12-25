228603c936a59dfce5f7e7ee04974348cfc549d7
tammarGrossman <tammargrossman@gmail.com>
2023-12-25 15:18:33 +0200

    sign up

--- client/src/pages/AddCourse.jsx
+++ client/src/pages/AddCourse.jsx
@@ -1,3 +1,170 @@
+// import React, { useState } from 'react';
+// import Box from '@mui/material/Box';
+// import TextField from '@mui/material/TextField';
+// import Button from '@mui/material/Button';
+// import Select from '@mui/material/Select';
+// import MenuItem from '@mui/material/MenuItem';
+// import FormControl from '@mui/material/FormControl';
+// import InputLabel from '@mui/material/InputLabel';
+// import Typography from '@mui/material/Typography';
+// import AddIcon from '@mui/icons-material/Add';
+// import PeopleIcon from '@mui/icons-material/People';
+// import MailOutlineIcon from '@mui/icons-material/MailOutline';
+// import AccessibilityNewIcon from '@mui/icons-material/AccessibilityNew';
+// import Zoom from '@mui/material/Zoom';
+
+// const AddCourse = () => {
+//   const [course, setCourse] = useState({
+//     name: '', // שדה השם קיים מהתחלה
+//     type: '',
+//     permission: '',
+//     inviteUsers: false,
+//     userToAdd: '',
+//   });
+
+//   const [animated, setAnimated] = useState(false);
+
+//   const handleAddCourse = () => {
+//     setAnimated(true);
+//     // כאן תוכל להוסיף לוגיקה להוספת הקורס למערכת
+//     // וגם להוספת משתמשים אם inviteUsers הוא true
+//     console.log('Adding course:', course);
+//   };
+
+//   const handleChange = (field, value) => {
+//     setCourse((prevCourse) => ({ ...prevCourse, [field]: value }));
+//   };
+
+//   return (
+//     <Box
+//       sx={{
+//         display: 'flex',
+//         flexDirection: 'column',
+//         alignItems: 'center',
+//         marginTop: '30px',
+//         fontFamily: 'Arial, sans-serif',
+//         padding: '20px',
+//         borderRadius: '10px',
+//         boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+//         backgroundColor: '#FFF',
+//       }}
+//     >
+//       <Typography variant="h4" sx={{ marginBottom: '20px', color: '#333', display: 'flex', alignItems: 'center' }}>
+//         <AddIcon fontSize="large" sx={{ marginRight: '10px', color: '#FF4081' }} />
+//         הוספת קורס
+//       </Typography>
+//       <TextField
+//         label="שם קורס"
+//         variant="outlined"
+//         value={course.name}
+//         onChange={(e) => handleChange('name', e.target.value)}
+//         sx={{ marginBottom: '20px', width: '300px' }}
+//         InputProps={{
+//           startAdornment: (
+//             <AccessibilityNewIcon sx={{ color: '#FF4081' }} />
+//           ),
+//         }}
+//       />
+//       <FormControl variant="outlined" sx={{ marginBottom: '20px', width: '300px' }}>
+//         <InputLabel>אופי הקורס</InputLabel>
+//         <Select
+//           value={course.type}
+//           onChange={(e) => handleChange('type', e.target.value)}
+//           label="אופי הקורס"
+//           sx={{ color: '#2196F3' }}
+//           inputProps={{
+//             startAdornment: (
+//               <PeopleIcon sx={{ color: '#2196F3' }} />
+//             ),
+//           }}
+//         >
+//           <MenuItem value="חוויתי">חוויתי</MenuItem>
+//           <MenuItem value="לימודי">לימודי</MenuItem>
+//           <MenuItem value="העשרה">העשרה</MenuItem>
+//         </Select>
+//       </FormControl>
+//       <FormControl variant="outlined" sx={{ marginBottom: '20px', width: '300px' }}>
+//         <InputLabel>הרשאת גישה לקורס</InputLabel>
+//         <Select
+//           value={course.permission}
+//           onChange={(e) => handleChange('permission', e.target.value)}
+//           label="הרשאת גישה לקורס"
+//           sx={{ color: '#4CAF50' }}
+//           inputProps={{
+//             startAdornment: (
+//               <MailOutlineIcon sx={{ color: '#4CAF50' }} />
+//             ),
+//           }}
+//         >
+//           <MenuItem value="פרטי">פרטי</MenuItem>
+//           <MenuItem value="ציבורי">ציבורי</MenuItem>
+//         </Select>
+//       </FormControl>
+//       {course.permission === 'פרטי' && (
+//         <FormControl sx={{ marginBottom: '20px', width: '300px' }}>
+//           <InputLabel>הזמן משתמשים לקורס</InputLabel>
+//           <Select
+//             value={course.inviteUsers}
+//             onChange={(e) => handleChange('inviteUsers', e.target.value)}
+//             label="הזמן משתמשים לקורס"
+//             sx={{ color: '#FFA000' }}
+//             inputProps={{
+//               startAdornment: (
+//                 <MailOutlineIcon sx={{ color: '#FFA000' }} />
+//               ),
+//             }}
+//           >
+//             <MenuItem value={true}>כן</MenuItem>
+//             <MenuItem value={false}>לא</MenuItem>
+//           </Select>
+//         </FormControl>
+//       )}
+//       {course.inviteUsers && (
+//         <TextField
+//           label="שם משתמש להזמנה"
+//           variant="outlined"
+//           value={course.userToAdd}
+//           onChange={(e) => handleChange('userToAdd', e.target.value)}
+//           sx={{ marginBottom: '20px', width: '300px' }}
+//           InputProps={{
+//             startAdornment: (
+//               <PeopleIcon sx={{ color: '#8BC34A' }} />
+//             ),
+//           }}
+//         />
+//       )}
+//       <Button
+//         variant="contained"
+//         onClick={handleAddCourse}
+//         sx={{ backgroundColor: '#FF4081', color: '#fff', marginBottom: '20px' }}
+//         startIcon={<AccessibilityNewIcon />}
+//       >
+//         הוסף קורס
+//       </Button>
+//       {course.inviteUsers && (
+//         <Button
+//           variant="contained"
+//           sx={{ backgroundColor: '#81C784', color: '#fff', width: '300px' }}
+//           startIcon={<PeopleIcon />}
+//         >
+//           הזמן משתמשים
+//         </Button>
+//       )}
+//       {course.inviteUsers && (
+//         <Button
+//           variant="contained"
+//           sx={{ backgroundColor: '#4CAF50', color: '#fff', width: '300px', marginTop: '20px' }}
+//           startIcon={<MailOutlineIcon />}
+//         >
+//           שלח מייל
+//         </Button>
+//       )}
+//     </Box>
+//   );
+// };
+
+// export default AddCourse;
+
 import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
@@ -13,7 +180,7 @@ import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import AccessibilityNewIcon from '@mui/icons-material/AccessibilityNew';
 import Zoom from '@mui/material/Zoom';
 
-const AddCourse = () => {
+const AddCourse = ({ spaceId }) => {
   const [course, setCourse] = useState({
     name: '',
     type: '',
@@ -26,9 +193,9 @@ const AddCourse = () => {
 
   const handleAddCourse = () => {
     setAnimated(true);
-    // כאן תוכל להוסיף לוגיקה להוספת הקורס למערכת
+    // כאן תוכלי להשתמש ב-mutation להוספת הקורס למערכת
     // וגם להוספת משתמשים אם inviteUsers הוא true
-    console.log('Adding course:', course);
+    console.log('Adding course to space:', spaceId, course);
   };
 
   const handleChange = (field, value) => {
@@ -46,27 +213,25 @@ const AddCourse = () => {
         padding: '20px',
         borderRadius: '10px',
         boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-        backgroundColor: '#FFF', // רקע לבן
+        backgroundColor: '#FFF',
       }}
     >
       <Typography variant="h4" sx={{ marginBottom: '20px', color: '#333', display: 'flex', alignItems: 'center' }}>
         <AddIcon fontSize="large" sx={{ marginRight: '10px', color: '#FF4081' }} />
         הוספת קורס
       </Typography>
-      <Zoom in={animated} timeout={500}>
-        <TextField
-          label="שם קורס"
-          variant="outlined"
-          value={course.name}
-          onChange={(e) => handleChange('name', e.target.value)}
-          sx={{ marginBottom: '20px', width: '300px' }}
-          InputProps={{
-            startAdornment: (
-              <AccessibilityNewIcon sx={{ color: '#FF4081' }} />
-            ),
-          }}
-        />
-      </Zoom>
+      <TextField
+        label="שם קורס"
+        variant="outlined"
+        value={course.name}
+        onChange={(e) => handleChange('name', e.target.value)}
+        sx={{ marginBottom: '20px', width: '300px' }}
+        InputProps={{
+          startAdornment: (
+            <AccessibilityNewIcon sx={{ color: '#FF4081' }} />
+          ),
+        }}
+      />
       <FormControl variant="outlined" sx={{ marginBottom: '20px', width: '300px' }}>
         <InputLabel>אופי הקורס</InputLabel>
         <Select
@@ -139,7 +304,7 @@ const AddCourse = () => {
         variant="contained"
         onClick={handleAddCourse}
         sx={{ backgroundColor: '#FF4081', color: '#fff', marginBottom: '20px' }}
-        startIcon={<AccessibilityNewIcon />} // אייקון של אדם
+        startIcon={<AccessibilityNewIcon />}
       >
         הוסף קורס
       </Button>
@@ -147,7 +312,7 @@ const AddCourse = () => {
         <Button
           variant="contained"
           sx={{ backgroundColor: '#81C784', color: '#fff', width: '300px' }}
-          startIcon={<PeopleIcon />} // אייקון של קבוצה
+          startIcon={<PeopleIcon />}
         >
           הזמן משתמשים
         </Button>
@@ -156,7 +321,7 @@ const AddCourse = () => {
         <Button
           variant="contained"
           sx={{ backgroundColor: '#4CAF50', color: '#fff', width: '300px', marginTop: '20px' }}
-          startIcon={<MailOutlineIcon />} // אייקון של מייל
+          startIcon={<MailOutlineIcon />}
         >
           שלח מייל
         </Button>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,20 @@
 import React, { useState } from 'react';
-import { Container, Typography, TextField, Button, Grid, Paper } from '@mui/material';
+import {
+  Container,
+  Typography,
+  TextField,
+  Button,
+  Grid,
+  Paper,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+} from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone';
+import SmsIcon from '@mui/icons-material/Sms';
 
 const theme = createTheme({
   palette: {
@@ -15,6 +29,9 @@ const SignUp = () => {
     email: '',
     password: '',
     confirmPassword: '',
+    userName: '',
+    phone: '',
+    howToContact: '',
   });
 
   const handleChange = (e) => {
@@ -22,10 +39,30 @@ const SignUp = () => {
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add your signup logic here using formData
-    console.log('Signup Form Data:', formData);
+  const handleSubmit = async (e) => {
+   
+      e.preventDefault();
+      const { confirmPassword, ...formDataWithoutConfirmPassword } = formData;
+    
+      try {
+        const response = await fetch('http://localhost:3000/users/createUser', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formDataWithoutConfirmPassword),
+        });
+    
+        if (response.ok) {
+          console.log('Signup successful!');
+        } else {
+          console.error('Signup failed:', response.status, response.statusText);
+        }
+      } catch (error) {
+        console.error('An error occurred during signup:', error);
+      }
+    
+    
   };
 
   return (
@@ -36,6 +73,7 @@ const SignUp = () => {
             Sign Up
           </Typography>
           <form onSubmit={handleSubmit} style={{ width: '100%' }}>
+           
             <Grid container spacing={2}>
               <Grid item xs={12}>
                 <TextField
@@ -59,6 +97,7 @@ const SignUp = () => {
                   onChange={handleChange}
                 />
               </Grid>
+
               <Grid item xs={12}>
                 <TextField
                   label="Confirm Password"
@@ -69,6 +108,56 @@ const SignUp = () => {
                   value={formData.confirmPassword}
                   onChange={handleChange}
                 />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  label="userName"
+                  type="text"
+                  fullWidth
+                  required
+                  name="userName"
+                  value={formData.userName}
+                  onChange={handleChange}
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  label="phone"
+                  type="text"
+                  fullWidth
+                  required
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleChange}
+                />
+              </Grid>
+
+
+              <Grid item xs={12}>
+                <FormControl fullWidth required>
+                  <InputLabel>How would you like us to contact you?</InputLabel>
+                  <Select
+                    name="howToContact"
+                    value={formData.howToContact}
+                    onChange={handleChange}
+                    label="How would you like us to contact you?"
+                  >
+                    <MenuItem value="email">
+                      <EmailIcon style={{ marginRight: '8px' }} />
+                      Email
+                    </MenuItem>
+                    <MenuItem value="phone">
+                      <PhoneIcon style={{ marginRight: '8px' }} />
+                      Voice Call
+                    </MenuItem>
+                    <MenuItem value="sms">
+                      <SmsIcon style={{ marginRight: '8px' }} />
+                      SMS
+                    </MenuItem>
+                  </Select>
+                </FormControl>
               </Grid>
             </Grid>
             <Button type="submit" variant="contained" color="primary" fullWidth style={{ marginTop: 20 }}>
